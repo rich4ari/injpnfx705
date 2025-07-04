38f39f8549c583e6d43c3f6480b2fe14c5f3411f
parudia13 <parudia13@gmail.com>
2025-07-05 01:52:00 +0900

    Updated AffiliatesTable.tsx

--- src/components/affiliate/admin/AffiliatesTable.tsx
+++ src/components/affiliate/admin/AffiliatesTable.tsx
@@ -84,6 +84,8 @@ const AffiliatesTable = () => {
             <div className="flex items-center">
               <Users className="w-5 h-5 mr-2" />
               <span>Daftar Affiliate ({affiliates.length})</span>
+            </div>
+          </div>
           <div className="flex flex-col sm:flex-row gap-2 sm:items-center">
             <Select value={selectedMonth} onValueChange={setSelectedMonth}>
               <SelectTrigger className="w-full sm:w-48">
