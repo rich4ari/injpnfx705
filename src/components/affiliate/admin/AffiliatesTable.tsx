e780f0a2e51b93ac8bbcf0f4075bb4dfe6fce63b
parudia13 <parudia13@gmail.com>
2025-07-05 01:52:51 +0900

    Fix missing CardTitle closing tag

--- src/components/affiliate/admin/AffiliatesTable.tsx
+++ src/components/affiliate/admin/AffiliatesTable.tsx
@@ -85,7 +85,7 @@ const AffiliatesTable = () => {
               <Users className="w-5 h-5 mr-2" />
               <span>Daftar Affiliate ({affiliates.length})</span>
             </div>
-          </div>
+          </CardTitle>
           <div className="flex flex-col sm:flex-row gap-2 sm:items-center">
             <Select value={selectedMonth} onValueChange={setSelectedMonth}>
               <SelectTrigger className="w-full sm:w-48">
